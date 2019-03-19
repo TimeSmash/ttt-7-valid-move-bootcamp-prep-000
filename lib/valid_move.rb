7f9ff3f35aa8446fca44251d6e8bc78d505577bc
mcumm64 <mcumm64@gmail.com>
2019-03-19 22:03:58 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -16,7 +16,7 @@ def valid_move?(board, index)
   end
 end
 
-def position_taken(board, index)
+def position_taken?(board, index)
   if (board[index] == " " || board[index] == "" || board[index] == nil) 
     return false
   elsif (board[index] == "X" || board[index] == "O") 
