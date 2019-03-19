750816b49ecbc8b8e82c2cc8726a5b575699fa44
mcumm64 <mcumm64@gmail.com>
2019-03-19 21:56:42 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,12 +4,16 @@
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
 def valid_move?(board, index) 
-  #is the move a position on the game board? Board has cells 1-9, which means an array from 0-8.
-  if (board[index] <= 8) {
+  #is the move a position on the game board? Board has cells 1-9, which means an array from 0-8
+  if position_taken?
+     if (board[index] <= 8) {
     return true
   } else {
     return false
   }
+  else
+    return false
+  end
 end
 
 def position_taken(board, index)
